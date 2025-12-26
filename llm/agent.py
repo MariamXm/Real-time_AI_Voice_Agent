15182be669bed676bcb249efc11768a2e35f35ff
Mariam Mahmood <157241258+MariamXm@users.noreply.github.com>
2025-12-27 00:17:31 +0500

    updated

--- llm/agent.py
+++ llm/agent.py
@@ -1,22 +1,3 @@
-# import os
-# from dotenv import load_dotenv
-# from groq import Groq
-
-# load_dotenv()
-
-# client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
-# def get_groq_response(prompt):
-#     chat_completion = client.chat.completions.create(
-#         messages=[
-#             {"role": "system", "content": "You are a helpful assistant."},
-#             {"role": "user", "content": prompt},
-#         ],
-#         model="llama-3.3-70b-versatile",
-#         max_tokens=520
-#     )
-#     return chat_completion.choices[0].message.content
-
 import os
 from dotenv import load_dotenv  # Import dotenv
 from groq import Groq
--- tts/speaker.py
+++ tts/speaker.py
@@ -1,22 +1,5 @@
-# speaker.py
-# import edge_tts
-# import asyncio
-
-# async def generate_tts(text, voice="en-US-GuyNeural", filename="response.wav"):
-#     """
-#     Converts text to speech and saves as WAV.
-#     Removes unnecessary line breaks or separators.
-#     """
-#     # Clean text: remove extra = signs, long dashes, multiple newlines
-#     clean_text = text.replace("=", "").replace("\n", " ").strip()
-#     communicate = edge_tts.Communicate(clean_text, voice)
-#     await communicate.save(filename)
-
-# def speak_text(text, filename="response.wav"):
-#     asyncio.run(generate_tts(text, filename=filename))
-
 import edge_tts
-import asyncio
+# import asyncio
 
 async def generate_tts(text, voice="en-US-GuyNeural", filename="response.wav"):
     """
